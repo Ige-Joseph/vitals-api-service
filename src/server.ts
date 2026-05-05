3bbaa79a764235b95ed32b171c63ff24bb3c474b
Ige-Joseph <119541924+Ige-Joseph@users.noreply.github.com>
2026-05-05 23:13:53 +0100

    chore: Update server
    Refactor server port configuration to use environment variable.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app";
 import { env } from "./config/env";
 import { logger } from "./config/logger";
 
-app.listen(env.PORT, () => {
-  logger.info(`Server running on port ${env.PORT}`);
-  logger.info(`Swagger docs available at http://localhost:${env.PORT}/api/docs`);
+const PORT = process.env.PORT || env.PORT || 3000;
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
 });
